0017beceb933d6512644ae28ae657a7542b5cfc8
bizet <176539309@qq.com>
2014-09-11 00:02:15 +0800

    ci

--- home/model/platform.model.js
+++ home/model/platform.model.js
@@ -13,13 +13,23 @@ var Platform = connection.model('platform', new mongoose.Schema({
   update_time: {type: Date, default: Date.now },
 }));
 
-var update = exports.update = function(_platforms) {
-  for (var i = _platforms.length - 1; i >= 0; i--) {
+var update = exports.update = function(_platforms, next) {
+  var count = 0;
+  var platform_count = _platforms.length;
+  if (platform_count == 0) {
+    next();
+    return;
+  }
+  for (var i = platform_count - 1; i >= 0; i--) {
     update_platform(_platforms[i], function(err){
+      count++;
       if (err) {
         console.log(err);
         return;
       }
+      if (count == platform_count) {
+        next();
+      };
     });
   };
 };
@@ -28,12 +38,12 @@ var update_platform = exports.update_platform = function(_platform, next) {
   if (!_platform.name) {
     next(new Error('no platform name'));
     return;
-  }
+  };
   Platform.findOneAndUpdate({name: _platform.name}, {$set: _platform}, {
     upsert: true
   }, function(err, doc) {
     next(err, doc);
-  })
+  });
 };
 
 var find_one = exports.find_one = function(name, next) {
--- home/test/platform.test.js
+++ home/test/platform.test.js
@@ -4,12 +4,20 @@ exports.testAdd = function(test) {
   {
     name: 'platform1',
     cem1_status: 'ok'
-  }
-  ]);
-  platform.find_one('platform1', function(err, doc) {
-    test.ifError(err);
-    test.equal(doc.cem1_status, 'ok');
-    test.done();
+  },
+  {
+    name: 'platform2',
+    cem1_status: 'ok'
+  },
+  ], function() {
+    platform.find_one('platform1', function(err, doc) {
+      test.ifError(err);
+      test.equal(doc.cem1_status, 'ok');
+      platform.find_one('platform2', function(err, doc) {
+        test.ifError(err);
+        test.equal(doc.cem1_status, 'ok');
+        test.done();
+      });
+    });
   });
-
 };
