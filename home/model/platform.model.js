a8d4bf943726d6be3365a2e7cbf6db529b87db22
bizet <176539309@qq.com>
2014-09-10 16:25:28 +0800

    model   platform

--- home/model/platform.model.js
+++ home/model/platform.model.js
@@ -3,4 +3,25 @@ var mongoose = require('mongoose'),
 
 var Platform = mongoose.model('platform', {
   name: String,
-})
+  ccm_status: String,
+  cem1_status: String,
+  cem2_status: String,
+  cem3_status: String,
+  soc_status: String,
+  type: String,
+  location: String,
+  update_time: {type: Date, default: Date.now },
+});
+
+var update = exports.update = function(_platform, next) {
+  if (!_platform.name) {
+    next(new Error('no platform name'));
+    return;
+  }
+  var platform = new Platform();
+  platform.findOneAndUpdate({name: _platform.name}, {$set: _platform}, {
+    upsert: true
+  }, function(err, doc) {
+    next(err, doc);
+  })
+};
